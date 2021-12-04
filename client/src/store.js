afe095970cc3af9f2779a45dfd7d25abf1214e56
ben16-creator <benkagia16@gmail.com>
2021-12-05 00:32:32 +0300

    productList reducer and actions

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { productListReducer } from "./reducers/productReducers";
 import { composeWithDevTools } from "redux-devtools-extension";
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
