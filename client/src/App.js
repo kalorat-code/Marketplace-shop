656f016a983d2a0fa09185be02a7ee55deccaba7
ben16-creator <benkagia16@gmail.com>
2021-12-06 22:41:31 +0300

    adding quantity to cart component

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-
+import CartScreen from "./screens/CartScreen";
 function App() {
   return (
     <BrowserRouter>
@@ -15,6 +15,7 @@ function App() {
           <Switch>
             <Route path="/" exact component={HomeScreen} />
             <Route path="/product/:id" exact component={ProductScreen} />
+            <Route path="/cart/:id?" exact component={CartScreen} />
           </Switch>
         </Container>
       </main>
