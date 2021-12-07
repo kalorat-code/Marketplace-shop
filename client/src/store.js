14ec353b8b8281039732d7136364b6775bd3c463
ben16-creator <benkagia16@gmail.com>
2021-12-07 12:00:24 +0300

    implementing cart reducer and add to cart action

--- client/src/store.js
+++ client/src/store.js
@@ -5,11 +5,23 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { cartReducer } from "./reducers/cartReducers";
+
+//
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
-const initialState = {};
+//
+const cartItemsFromStorange = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorange,
+  },
+};
 const middleware = [thunk];
 const store = createStore(
   reducer,
