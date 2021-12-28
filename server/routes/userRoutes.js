0bc9effb3a0538428b8752d3967d20bdcd536985
ben16-creator <benkagia16@gmail.com>
2021-12-28 23:25:36 +0300

    user registration and password encryption

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
 const router = express.Router();
 // import { authUser } from '../controllers/users.js'
-import { authUser, getUserProfile } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
 import { protect } from "../middlewares/generateToken.js.js";
 
+router.route("/").post(registerUser);
 router.post("/login", authUser);
-// router.route("/profile").get(protect, getUserProfile);
+
 router.router("/profile").get(protect, getUserProfile);
 
 export default router;
